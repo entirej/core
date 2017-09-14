ea9bed5bea165c6f48c88e90b824c7be82f8423f
Anuradha Gunasekara <theanuradha@gmail.com>
2017-09-14 18:51:06 +0530

    header change to cresoft

--- entirej-core/src/org/entirej/framework/core/data/EJDataForm.java
+++ entirej-core/src/org/entirej/framework/core/data/EJDataForm.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2013 Mojave Innovations GmbH
+ * Copyright 2013 CRESOFT AG
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  * 
  * Contributors:
- *     Mojave Innovations GmbH - initial API and implementation
+ *     CRESOFT AG - initial API and implementation
  ******************************************************************************/
 package org.entirej.framework.core.data;
 
--- entirej-core/src/org/entirej/framework/core/properties/reader/EJCoreDrawerPagePropertiesHandler.java
+++ entirej-core/src/org/entirej/framework/core/properties/reader/EJCoreDrawerPagePropertiesHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2013 Mojave Innovations GmbH
+ * Copyright 2013 CRESOFT AG
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  * 
- * Contributors: Mojave Innovations GmbH - initial API and implementation
+ * Contributors: CRESOFT AG - initial API and implementation
  ******************************************************************************/
 package org.entirej.framework.core.properties.reader;
 
