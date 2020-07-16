1b36312a02c451be1aa1620caffc3b4e4a486aef
Anuradha Gunasekara <theanuradha@gmail.com>
2020-07-16 16:12:46 +0530

    added call back with error

--- entirej-core/src/org/entirej/framework/core/EJAsyncCallback.java
+++ entirej-core/src/org/entirej/framework/core/EJAsyncCallback.java
@@ -3,4 +3,9 @@ package org.entirej.framework.core;
 public interface EJAsyncCallback<T>
 {
     void completed(EJFrameworkHelper helper, T result);
+    
+    default void completedWithError(EJFrameworkHelper helper , Exception t) 
+    {
+        helper.handleException(t);
+    }
 }
